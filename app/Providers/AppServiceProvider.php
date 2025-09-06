70cae2b7fa8fa9e53db6e65548a6c90c3d2409ab
unknown <tonyentertain@gmail.com>
2025-09-07 00:06:13 +0100

    Add permission for admin in Log Viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LogViewer::auth(function ($request) {
+            return $request->user() && in_array($request->user()->email, [
+                config('mine.admin_email'),
+            ]);
+         });
     }
 }
